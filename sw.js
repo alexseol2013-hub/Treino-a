03e21958face535eb3207cab657df82781f52b90
alexseol2013-hub <alexseol2013@gmail.com>
2026-08-04 21:52:43 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,3 +0,0 @@
-const CACHE="treino-a-v1";const ASSETS=["./","./index.html","./app.js","./manifest.json","./icon.svg"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
